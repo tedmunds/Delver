0ef9e0639c9e7dc772074fd32c6c2ed038f0caf2
Michael Hofer <mhofer180993@gmail.com>
2017-08-07 15:31:37 -0700

    Adds missing files from previous commit

---
+++ Delver/Assets/Source/Gameplay/BaseModifier.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(menuName = "Modifiers/BaseModifier")]
+public class BaseModifier : ScriptableObject
+{
+
+    [SerializeField]
+    public float value;
+
+    [SerializeField]
+    public float lifetime = -1;
+
+    [SerializeField]
+    public string tag;
+
+    [SerializeField]
+    public ModifierTarget target;
+   
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public ModifierContainer.Modifier getModifier()
+    {
+        return ModifierContainer.MakeModifier(tag, value, lifetime);
+    }
+}
